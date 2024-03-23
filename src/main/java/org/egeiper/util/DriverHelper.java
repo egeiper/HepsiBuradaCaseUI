0263c7e3db098fcc055dba99355557dd494dc11c
Ege Iper <ege.iper@launchmetrics.com>
2024-03-23 15:25:30 +0300

    initial commit

--- src/main/java/org/egeiper/util/DriverHelper.java
+++ src/main/java/org/egeiper/util/DriverHelper.java
@@ -3,18 +3,15 @@ package org.egeiper.util;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class DriverHelper {
 
-    private ThreadLocal<WebDriver> DRIVER = new ThreadLocal<>();
+    private final ThreadLocal<WebDriver> DRIVER = new ThreadLocal<>();
     private static final String GRID_URL = PropertyUtils.getProperty("test.properties", "gridURL");
 
     public WebDriver initializeDriver() throws MalformedURLException {
@@ -28,20 +25,19 @@ public class DriverHelper {
         return getDriver();
     }
 
-    private WebDriver createLocalDriver() {
-        return new ChromeDriver(getChromeOptions());
-    }
-
-    private static ChromeOptions getChromeOptions() {
-        final Map<String, Object> prefs = new HashMap<>();
-        final ChromeOptions chromeOpts = new ChromeOptions();
-        chromeOpts.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
-        chromeOpts.setExperimentalOption("prefs", prefs);
-        return chromeOpts;
-    }
-
     private WebDriver createRemoteDriver() throws MalformedURLException {
         return new RemoteWebDriver(new URL(GRID_URL), getChromeOptions());
+    }
+
+    public ChromeOptions getChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        final String serverProperty = System.getProperty("SERVER");
+        if ("actions".equals(serverProperty)) {
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--headless");
+        }
+        return options;
     }
 
     protected WebDriver getDriver() {
