91939ad2e93c7798a295431506e63aa23302dc81
Ege Iper <ege.iper@launchmetrics.com>
2024-03-23 15:49:31 +0300

    initial commit

--- src/main/java/org/egeiper/util/DriverHelper.java
+++ src/main/java/org/egeiper/util/DriverHelper.java
@@ -32,21 +32,17 @@ public class DriverHelper {
     public ChromeOptions getChromeOptions() {
         ChromeOptions options = new ChromeOptions();
         final String serverProperty = System.getProperty("SERVER");
-        options.addArguments("--window-size=1920,1080");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--proxy-server='direct://'");
-        options.addArguments("--proxy-bypass-list=*");
-        options.addArguments("--start-maximized");
-        //options.addArguments("--headless");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--ignore-certificate-errors");
         if ("actions".equals(serverProperty)) {
-            options.addArguments("--no-sandbox");
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--disable-extensions");
+            options.addArguments("--proxy-server='direct://'");
+            options.addArguments("--proxy-bypass-list=*");
+            options.addArguments("--start-maximized");
+            //options.addArguments("--headless");
+            options.addArguments("--disable-gpu");
             options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--headless");
-            options.addArguments("window-size=1200,1100");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--ignore-certificate-errors");
         }
         return options;
     }
